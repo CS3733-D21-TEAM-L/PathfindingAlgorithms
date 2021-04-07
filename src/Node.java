6133aa9f77e7c7a6251d6d2b410f6382470a8694
fmcshan <fmcshan@wpi.edu>
2021-04-06 20:09:09 -0400

    Updated visited field to have public visibility

--- src/Node.java
+++ src/Node.java
@@ -6,7 +6,7 @@ public class Node {
     private Node parent; //for backtracking
     private String fullName; //Node's full name
     private String shortName; //Node's short name
-    private boolean visited; //Boolean used to check if the node has been visited or not
+    public boolean visited; //Boolean used to check if the node has been visited or not
 
     public Node(String nodeID) {
         this.nodeID = nodeID;
